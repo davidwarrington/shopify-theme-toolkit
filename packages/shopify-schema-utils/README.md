da7ba7ef7230fc9b6c67fa5f38435ace669534b0
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2025-06-08 22:25:50 +0100

    feat(shopify-schema-utils): support type suggestions for locales

--- packages/shopify-schema-utils/README.md
+++ packages/shopify-schema-utils/README.md
@@ -35,3 +35,59 @@ export default defineBlockSchema({
   // ...
 });
 ```
+
+### Types
+
+Prefer not to use `define` functions? Need more granular types? This package exports a whole bunch of types for schemas too, including definitions for individual setting types, and schema presets.
+
+```ts
+import type { SelectSettingSchema } from 'shopify-schema-utils';
+
+export const colorsSetting: SelectSettingSchema = {
+  // ...
+};
+```
+
+## Editor suggestions
+
+`defineBlockSchema` and `defineSectionSchema` can be enhanced by passing your translations/locales as a generic.
+
+```ts
+import { defineBlockSchema } from 'shopify-schema-utils';
+import locales from '@locales/en.default.schema.json';
+
+export default defineBlockSchema<typeof locales>({
+  // Translatable keys such as name will have autocomplete suggestions in your editor
+  name: 't:',
+});
+```
+
+If you don't want to repeat the above boilerplate _every time_ you call `defineBlockSchema` or `defineSectionSchema`, you can create your own importable utilities like so
+
+```ts
+// schemas/utils/define.ts
+
+import {
+  defineBlockSchema as _defineBlockSchema,
+  defineSectionSchema as _defineSectionSchema,
+} from 'shopify-schema-utils';
+import locales from '@locales/en.default.schema.json';
+
+export const defineBlockSchema = _defineBlockSchema<typeof locales>;
+export const defineSectionSchema = _defineSectionSchema<typeof locales>;
+```
+
+Now, rather than importing the utilities directly from `shopify-schema-utils`, import the overridden ones from your `schemas/utils/define.ts` file.
+
+```ts
+// schemas/blocks/my-block.ts
+
+import { defineBlockSchema } from '../utils/define';
+
+export default defineBlockSchema({
+  // Translatable keys are suggested because `utils/define` has the translations preloaded
+  name: 't:',
+});
+```
+
+Even if you prefer JavaScript over TypeScript, I'd recommend using TS just for definition utilities as per the `utils/define.ts` example. Alternatively you can use JSDoc to get the same benefits.
--- packages/shopify-schema-utils/src/index.ts
+++ packages/shopify-schema-utils/src/index.ts
@@ -1,30 +1,58 @@
-interface BaseSettingSchema {
+type DotPrefix<T extends string> = T extends '' ? '' : `.${T}`;
+
+type DotNestedKeys<T> = (
+  T extends object
+    ? {
+        [K in Exclude<
+          keyof T,
+          symbol
+        >]: `${K}${DotPrefix<DotNestedKeys<T[K]>>}`;
+      }[Exclude<keyof T, symbol>]
+    : ''
+) extends infer D
+  ? Extract<D, string>
+  : never;
+
+type Translations<Dictionary> = DotNestedKeys<Dictionary>;
+type TranslationKey<Dictionary> = `t:${Translations<Dictionary>}`;
+
+type TranslationKeyOrString<Dictionary> =
+  | TranslationKey<Dictionary>
+  | (string & {});
+
+type Locales = Record<string, unknown>;
+
+interface BaseSettingSchema<Translations extends Locales> {
   readonly id: string;
-  readonly label: string;
-  readonly info?: string;
+  readonly label: TranslationKeyOrString<Translations>;
+  readonly info?: TranslationKeyOrString<Translations>;
   readonly visible_if?: string;
 }
 
-interface BaseMetaSchema {
-  readonly info?: string;
-  readonly content: string;
+interface BaseMetaSchema<Translations extends Locales> {
+  readonly info?: TranslationKeyOrString<Translations>;
+  readonly content: TranslationKeyOrString<Translations>;
   readonly visible_if?: string;
 }
 
-export interface HeaderSettingSchema extends BaseMetaSchema {
+export interface HeaderSettingSchema<Translations extends Locales>
+  extends BaseMetaSchema<Translations> {
   readonly type: 'header';
 }
 
-export interface ParagraphSettingSchema extends BaseMetaSchema {
+export interface ParagraphSettingSchema<Translations extends Locales>
+  extends BaseMetaSchema<Translations> {
   readonly type: 'paragraph';
 }
 
-export interface CheckboxSettingSchema extends BaseSettingSchema {
+export interface CheckboxSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'checkbox';
   readonly default?: boolean;
 }
 
-export interface NumberSettingSchema extends BaseSettingSchema {
+export interface NumberSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'number';
   readonly default?: number;
   readonly placeholder?: string;
@@ -35,13 +63,15 @@ export interface RadioOption {
   readonly label: string;
 }
 
-export interface RadioSettingSchema extends BaseSettingSchema {
+export interface RadioSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'radio';
   readonly options: RadioOption[];
   readonly default?: string;
 }
 
-export interface RangeSettingSchema extends BaseSettingSchema {
+export interface RangeSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'range';
   readonly min: number;
   readonly max: number;
@@ -56,167 +86,193 @@ interface SelectOption {
   readonly group: string;
 }
 
-export interface SelectSettingSchema extends BaseSettingSchema {
+export interface SelectSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'select';
   readonly options: SelectOption[];
   readonly default?: string;
 }
 
-export interface TextSettingSchema extends BaseSettingSchema {
+export interface TextSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'text';
   readonly placeholder?: string;
   readonly default?: string;
 }
 
-export interface TextAreaSettingSchema extends BaseSettingSchema {
+export interface TextAreaSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'textarea';
   readonly placeholder?: string;
   readonly default?: string;
 }
 
-export interface ArticleSettingSchema extends BaseSettingSchema {
+export interface ArticleSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'article';
 }
 
-export interface BlogSettingSchema extends BaseSettingSchema {
+export interface BlogSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'blog';
 }
 
-export interface CollectionSettingSchema extends BaseSettingSchema {
+export interface CollectionSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'collection';
 }
 
-export interface CollectionListSettingSchema extends BaseSettingSchema {
+export interface CollectionListSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'collection_list';
   readonly limit?: number;
 }
 
-export interface ColorSettingSchema extends BaseSettingSchema {
+export interface ColorSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'color';
   readonly default?: string;
   readonly alpha?: boolean;
 }
 
-export interface ColorBackgroundSettingSchema extends BaseSettingSchema {
+export interface ColorBackgroundSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'color_background';
   readonly default?: string;
 }
 
-export interface ColorSchemeSettingSchema extends BaseSettingSchema {
+export interface ColorSchemeSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'color_scheme';
   readonly default?: string;
 }
 
-export interface ColorSchemaGroupSettingSchema extends BaseSettingSchema {
+export interface ColorSchemaGroupSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'color_scheme_group';
 }
 
-export interface FontPickerSettingSchema extends BaseSettingSchema {
+export interface FontPickerSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'font_picker';
   readonly default?: string;
 }
 
-export interface HtmlSettingSchema extends BaseSettingSchema {
+export interface HtmlSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'html';
   readonly default?: string;
 }
 
-export interface ImagePickerSettingSchema extends BaseSettingSchema {
+export interface ImagePickerSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'image_picker';
 }
 
-export interface InlineRichtextSettingSchema extends BaseSettingSchema {
+export interface InlineRichtextSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'inline_richtext';
   readonly default?: string;
 }
 
-export interface LiquidSettingSchema extends BaseSettingSchema {
+export interface LiquidSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'liquid';
   readonly default?: string;
 }
 
-export interface MetaobjectSettingSchema extends BaseSettingSchema {
+export interface MetaobjectSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'metaobject';
   readonly metaobject_type?: string;
 }
 
-export interface MetaobjectListSettingSchema extends BaseSettingSchema {
+export interface MetaobjectListSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'metaobject';
   readonly metaobject_type?: string;
   readonly limit?: number;
 }
 
-export interface PageSettingSchema extends BaseSettingSchema {
+export interface PageSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'page';
 }
 
-export interface ProductSettingSchema extends BaseSettingSchema {
+export interface ProductSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'product';
 }
 
-export interface ProductListSettingSchema extends BaseSettingSchema {
+export interface ProductListSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'product_list';
   readonly limit?: number;
 }
 
-export interface RichTextSettingSchema extends BaseSettingSchema {
+export interface RichTextSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'richtext';
   readonly default?: string;
 }
 
-export interface TextAlignmentSettingSchema extends BaseSettingSchema {
+export interface TextAlignmentSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'text_alignment';
   readonly default?: 'left' | 'center' | 'right';
 }
 
-export interface UrlSettingSchema extends BaseSettingSchema {
+export interface UrlSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'url';
   readonly default?: string;
 }
 
-export interface VideoSettingSchema extends BaseSettingSchema {
+export interface VideoSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'video';
 }
 
-export interface VideoUrlSettingSchema extends BaseSettingSchema {
+export interface VideoUrlSettingSchema<Translations extends Locales>
+  extends BaseSettingSchema<Translations> {
   readonly type: 'video_url';
   readonly accept: ('vimeo' | 'youtube')[];
   readonly placeholder?: string;
 }
 
-export type SettingSchema =
-  | HeaderSettingSchema
-  | ParagraphSettingSchema
-  | CheckboxSettingSchema
-  | NumberSettingSchema
-  | RadioSettingSchema
-  | RangeSettingSchema
-  | SelectSettingSchema
-  | TextSettingSchema
-  | TextAreaSettingSchema
-  | ArticleSettingSchema
-  | BlogSettingSchema
-  | CollectionSettingSchema
-  | CollectionListSettingSchema
-  | ColorSettingSchema
-  | ColorBackgroundSettingSchema
-  | ColorSchemeSettingSchema
-  | ColorSchemaGroupSettingSchema
-  | FontPickerSettingSchema
-  | HtmlSettingSchema
-  | ImagePickerSettingSchema
-  | InlineRichtextSettingSchema
-  | LiquidSettingSchema
-  | MetaobjectSettingSchema
-  | MetaobjectListSettingSchema
-  | PageSettingSchema
-  | ProductSettingSchema
-  | ProductListSettingSchema
-  | RichTextSettingSchema
-  | TextAlignmentSettingSchema
-  | UrlSettingSchema
-  | VideoSettingSchema
-  | VideoUrlSettingSchema;
+export type SettingSchema<Translations extends Locales> =
+  | HeaderSettingSchema<Translations>
+  | ParagraphSettingSchema<Translations>
+  | CheckboxSettingSchema<Translations>
+  | NumberSettingSchema<Translations>
+  | RadioSettingSchema<Translations>
+  | RangeSettingSchema<Translations>
+  | SelectSettingSchema<Translations>
+  | TextSettingSchema<Translations>
+  | TextAreaSettingSchema<Translations>
+  | ArticleSettingSchema<Translations>
+  | BlogSettingSchema<Translations>
+  | CollectionSettingSchema<Translations>
+  | CollectionListSettingSchema<Translations>
+  | ColorSettingSchema<Translations>
+  | ColorBackgroundSettingSchema<Translations>
+  | ColorSchemeSettingSchema<Translations>
+  | ColorSchemaGroupSettingSchema<Translations>
+  | FontPickerSettingSchema<Translations>
+  | HtmlSettingSchema<Translations>
+  | ImagePickerSettingSchema<Translations>
+  | InlineRichtextSettingSchema<Translations>
+  | LiquidSettingSchema<Translations>
+  | MetaobjectSettingSchema<Translations>
+  | MetaobjectListSettingSchema<Translations>
+  | PageSettingSchema<Translations>
+  | ProductSettingSchema<Translations>
+  | ProductListSettingSchema<Translations>
+  | RichTextSettingSchema<Translations>
+  | TextAlignmentSettingSchema<Translations>
+  | UrlSettingSchema<Translations>
+  | VideoSettingSchema<Translations>
+  | VideoUrlSettingSchema<Translations>;
 
 export interface SchemaPresetBlock {
   readonly type: string;
@@ -228,7 +284,7 @@ export interface SchemaPresetBlock {
   readonly settings?: Record<string, unknown>;
 }
 
-export interface Schema {
+export interface Schema<Translations extends Locales> {
   readonly disabled_on?: {
     readonly groups?: string[];
     readonly templates?: string[];
@@ -237,36 +293,64 @@ export interface Schema {
     readonly groups?: string[];
     readonly templates?: string[];
   };
-  readonly name: string;
+  readonly name: TranslationKeyOrString<Translations>;
   readonly class?: string | null;
   readonly tag?: string | null;
   readonly blocks?: (
     | { type: string }
     | {
-        name: string;
+        name: TranslationKeyOrString<Translations>;
         type: string;
         limit?: number;
-        settings?: SettingSchema[];
+        settings?: SettingSchema<Translations>[];
       }
   )[];
   readonly max_blocks?: number;
   readonly limit?: number;
-  readonly settings?: SettingSchema[];
+  readonly settings?: SettingSchema<Translations>[];
   readonly presets?: {
-    name: string;
+    name: TranslationKeyOrString<Translations>;
     blocks?: Record<string, SchemaPresetBlock>;
     block_order?: string[];
     settings?: Record<string, unknown>;
-    category?: string;
+    category?: TranslationKeyOrString<Translations>;
   }[];
 }
 
-/** @todo narrow type */
-export function defineBlockSchema(schema: Schema) {
+/**
+ * Pass translations to function for type hints.
+ *
+ * @example
+ * import locales from '../locales/en.default.schema.json';
+ *
+ * export default defineBlockSchema<typeof locales>({
+ *  // Type hints will be shown for all translatable properties
+ *  name: 't:',
+ * })
+ *
+ * @todo narrow type
+ */
+export function defineBlockSchema<Translations extends Locales>(
+  schema: Schema<Translations>,
+) {
   return schema;
 }
 
-/** @todo narrow type */
-export function defineSectionSchema(schema: Schema) {
+/**
+ * Pass translations to function for type hints.
+ *
+ * @example
+ * import locales from '../locales/en.default.schema.json';
+ *
+ * export default defineSectionSchema<typeof locales>({
+ *  // Type hints will be shown for all translatable properties
+ *  name: 't:',
+ * })
+ *
+ * @todo narrow type
+ */
+export function defineSectionSchema<Translations extends Locales>(
+  schema: Schema<Translations>,
+) {
   return schema;
 }
